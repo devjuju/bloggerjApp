57ced33ef3ba26fcdc4fa3f5d13b134f5554e225
devjuju <jleleubellpro1994@gmail.com>
2025-05-22 15:33:38 +0200

    Fix: Open a static session for the user

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
 <?php
+session_start();
+// to display the error message
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+//require "src/core/router.php";
 require "../vendor/autoload.php";
 
 use App\Core\Router;
