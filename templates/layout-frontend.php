670878ffb1c5999d3b07ce12e53cdcec1dd6183b
devjuju <jleleubellpro1994@gmail.com>
2025-08-21 18:21:18 +0200

    require once

--- templates/layout-frontend.php
+++ templates/layout-frontend.php
@@ -30,13 +30,13 @@
 <body>
 
     <!-- Inclusion de la navigation du frontend -->
-    <?php require('../templates/navbar-frontend.php') ?>
+    <?php require_once __DIR__ . '/../templates/navbar-frontend.php'; ?>
 
     <!-- Contenu principal -->
     <?= $content ?>
 
     <!-- Inclusion du footer -->
-    <?php require('../templates/footer.php') ?>
+    <?php require_once __DIR__ . '/../templates/footer.php'; ?>
 
     <!-- JS Boostrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
